3b19433a156033e38d4ff6447ccc63f5d368ba6d
jhinelia <jhinelia@student.21-school.ru>
2025-11-17 17:39:37 +0300

    init terraform example

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+    description = "Access Key to access SberCloud"
+    sensitive   = true
+}
+
+variable "secret_key" {
+    description = "Secret Key to access SberCloud"
+    sensitive   = true
+}
---
+++ versions.tf
@@ -0,0 +1,9 @@
+terraform {
+    required_providers {
+        sbercloud = {
+            source = "sbercloud-terraform/sbercloud"
+            version = "1.12.14"
+        }
+    }
+    required_version = "1.13.5"
+}
